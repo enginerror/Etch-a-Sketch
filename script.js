89dae75cbfb9b86f4ffab9594b7d6f97a2f52053
ayaanmurshedkhan <ayaanmurshedkhan@gmail.com>
2025-01-24 08:07:59 +0530

    complete css

---
+++ script.js
@@ -0,0 +1,7 @@
+const sketch_area = document.querySelector("#sketch-area");
+
+const sketchAreaSize = 450;
+let sideValue = 16;
+
+sketch_area.style.height = `${sketchAreaSize}px`;
+sketch_area.style.width = `${sketchAreaSize}px`;
